300e7deae6c22862811aa352dee2fec5e28b9fe7
0xAnishDev <0xanish.dev@gmail.com>
2026-03-13 13:56:32 +0530

    crime based heatmap

--- static/script.js
+++ static/script.js
@@ -9,16 +9,35 @@ var map = L.map('map', {
 }).setView([22.9734, 78.6569], 5)
 
 var cityCoords = {
-    "Mumbai":[19.0760,72.8777],
-    "Delhi":[28.7041,77.1025],
-    "Bangalore":[12.9716,77.5946],
-    "Chennai":[13.0827,80.2707],
-    "Kolkata":[22.5726,88.3639],
-    "Hyderabad":[17.3850,78.4867],
     "Ahmedabad":[23.0225,72.5714],
+    "Chennai":[13.0827,80.2707],
+    "Ludhiana":[30.9010,75.8573],
     "Pune":[18.5204,73.8567],
+    "Delhi":[28.7041,77.1025],
+    "Mumbai":[19.0760,72.8777],
+    "Surat":[21.1702,72.8311],
+    "Visakhapatnam":[17.6868,83.2185],
+    "Bangalore":[12.9716,77.5946],
+    "Kolkata":[22.5726,88.3639],
+    "Ghaziabad":[28.6692,77.4538],
+    "Hyderabad":[17.3850,78.4867],
+    "Jaipur":[26.9124,75.7873],
+    "Lucknow":[26.8467,80.9462],
+    "Bhopal":[23.2599,77.4126],
+    "Patna":[25.5941,85.1376],
+    "Kanpur":[26.4499,80.3319],
+    "Varanasi":[25.3176,82.9739],
+    "Nagpur":[21.1458,79.0882],
+    "Meerut":[28.9845,77.7064],
+    "Thane":[19.2183,72.9781],
+    "Indore":[22.7196,75.8577],
+    "Rajkot":[22.3039,70.8022],
+    "Vasai":[19.4910,72.8054],
     "Agra":[27.1767,78.0081],
-    "Bhopal":[23.2599,77.4126]
+    "Kalyan":[19.2437,73.1355],
+    "Nashik":[19.9975,73.7898],
+    "Srinagar":[34.0837,74.7973],
+    "Faridabad":[28.4089,77.3178]
 }
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -62,7 +81,7 @@ console.log("Crime data:", data)
 function updateHeatmap(data){
 
     var points = []
-
+    
     for (var city in data){
 
         if(cityCoords[city]){
@@ -81,7 +100,7 @@ function updateHeatmap(data){
     map.removeLayer(heatLayer)
 
     heatLayer = L.heatLayer(points, {
-        radius: 50,
+        radius: 40,
         blur: 70,
         maxZoom: 9,
         gradient: {
@@ -92,7 +111,8 @@ function updateHeatmap(data){
             1.0: "#bd0026"
         }
     }).addTo(map)
-
+    console.log("City from dataset:", city)
+    console.log("Coords exist:", cityCoords[city])
 }
 
 document.getElementById("crimeSelect").dispatchEvent(new Event("change"))
